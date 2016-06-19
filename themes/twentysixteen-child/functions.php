094854d295ec705bf8a20ed1a40e8967a66f9076
mismith0227 <doraepon2216@gmail.com>
2016-06-19 16:07:54 +0900

    サンプルコードの追加

---
+++ themes/twentysixteen-child/functions.php
@@ -0,0 +1,5 @@
+<?php
+function theme_enqueue_styles() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
